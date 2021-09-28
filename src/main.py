d343c6dab0f7d4ff83645762ce75bd3fb101d333
gazelle51 <sarahgazelle@hotmail.com>
2021-09-28 18:53:00 +1000

    get order details and display

--- src/main.py
+++ src/main.py
@@ -128,12 +128,77 @@ try:
         # Load order confirmation into Beautiful Soup
         soup = BeautifulSoup(open('./src/bigw/order_confirmation.html', encoding="utf8"), "html.parser")
 
-    # TODO: get order details and save
+    # TODO: put this into its own function
+    # TODO: file logging
+
+    # Set up order data
+    order_data = ''
+
+    # Find tables
+    tables = soup.findAll('table')
+    for table in tables:
+        # Stop if not "Order Details"
+        if not table.findPreviousSibling().text.lower() == 'order details':
+            continue
+
+        # Update order data with table details
+        order_data = order_data + '-------------\nOrder details\n-------------\n\n'
+        rows = table.findAll('tr')
+        for row in rows:
+            table_data = row.findAll('td')
+            order_data = order_data + table_data[0].text.strip() + ': ' + table_data[1].text.strip() + '\n'
+
+        order_data = order_data + '\n'
+
+    # Find addresses
+    addresses = soup.findAll('address')
+    for address in addresses:
+        # Update order data with address details
+        order_data = order_data + '----------------\n' + address.findPreviousSibling().text.strip() + '\n----------------\n\n'
+        divs = address.findAll('div')
+        for div in divs:
+            order_data = order_data + div.text.strip() + '\n'
+
+        order_data = order_data + '\n'
+
+    # Find order summary
+    order_summary = soup.find('div', {'class': 'OrderConfirmationSummary'})
+
+    # Update order data with address details
+    order_data = order_data + '-------------\nOrder Summary\n-------------\n\n'
+    items = order_summary.findAll('div', {'class': 'CartSummaryEntryItem'})
+    for i, item in enumerate(items):
+        product = item.find('div', {'class': 'product'})
+        quantity = item.find('div', {'class': 'quantity'})
+        price = item.find('span', {'class': 'price'})
+        order_data = order_data + '{}. {} (x{}) {}\n'.format(i+1, product.text.strip(),
+                                                             quantity.text.strip(), price.text.strip().replace('\n', ''))
+
+    # Item subtotal
+    subtotal = soup.find('div', {'class': 'summary-detail'}) \
+        .find('div', {'class': 'CartSummaryRow'}) \
+        .find('span', {'class': 'Price'})
+    order_data = order_data + '\nItem subtotal: {}\n'.format(subtotal.text.strip().replace('\n', ''))
+
+    # Final total
+    total = soup.find('div', {'class': 'order-summary-total'}) \
+        .find('span', {'class': 'Price'})
+    order_data = order_data + 'Total after discounts and shipping: {}\n'.format(total.text.strip().replace('\n', ''))
+
+    order_data = order_data + '\n'
+
+    # TODO: save to file
+    print(order_data)
+
+    wd.save_screenshot('images/{}_finished.png'.format(datetime.now().strftime('%Y%m%d_%H%M%S')))
 
     # Quit Chrome
-    # wd.close()
-    # wd.quit()
+    wd.close()
+    wd.quit()
 
 except Exception as e:
     wd.save_screenshot('images/{}_error.png'.format(datetime.now().strftime('%Y%m%d_%H%M%S')))
+    # TODO: save HTML
+    wd.close()
+    wd.quit()
     raise e
