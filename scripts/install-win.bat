c0647134f9c958c1b374c5baea7116e9a995e86e
gazelle51 <48930996+gazelle51@users.noreply.github.com>
2021-10-07 20:14:38 +1100

    WIP: windows install script

---
+++ scripts/install-win.bat
@@ -0,0 +1,16 @@
+@REM Create a virtualenv if it doesn't exist 
+if not exist .venv\ (
+  python -m venv .venv
+  echo "Virtual environment created"
+)
+
+@REM Activate the virtualenv
+.venv\Scripts\activate.bat
+
+@REM Update pip
+python -m pip install --upgrade pip
+
+@REM Install requirements
+pip install -r ./requirements.txt 
+
+echo "Dependencies installed"
